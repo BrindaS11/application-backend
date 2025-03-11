a326f9e9b8d4b1fcebff11a09381a08cd34c5c63
EBY TOM <73706705+ebytom@users.noreply.github.com>
2025-03-11 14:39:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// api routes
+//routes
 app.use('/api/v1/app/auth', authRouter);
 app.use('/api/v1/app/users', usersRouter);
 app.use('/api/v1/app/address', addressRouter);
