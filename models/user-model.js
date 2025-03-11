a41a8cd92d17c4b533f6d890eb4543e4b891e2ad
EBY TOM <73706705+ebytom@users.noreply.github.com>
2025-03-11 15:37:41 +0530

    Update user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -4,6 +4,7 @@ module.exports = {
         id: "INT AUTO_INCREMENT PRIMARY KEY",
         name: "VARCHAR(255) NOT NULL",
         email: "VARCHAR(255) UNIQUE NOT NULL",
+        mobile: "VARCHAR(255) UNIQUE NOT NULL",
         created_at: "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
     }
 };
