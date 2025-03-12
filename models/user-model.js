d441127b5034608c548e95e73e19aa46e6ee77d9
BrindaS11 <85933206+BrindaS11@users.noreply.github.com>
2025-03-12 18:33:23 +0530

    Update user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -5,6 +5,7 @@ module.exports = {
         name: "VARCHAR(255) NOT NULL",
         email: "VARCHAR(255) UNIQUE NOT NULL",
         mobile: "VARCHAR(255) UNIQUE NOT NULL",
+        gender: "VARCHAR(255) UNIQUE NOT NULL",
         created_at: "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
     }
 };
