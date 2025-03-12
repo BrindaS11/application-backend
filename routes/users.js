c81d761cb72ead4820ef9ef2532f585ea6aa18c3
EBY TOM <73706705+ebytom@users.noreply.github.com>
2025-03-12 09:28:10 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { addUser, getUserById, getAllUsers } = require("../controllers/users");
 
-router.get('/addUser', addUser);
+router.post('/addUser', addUser);
 router.get('/getUserById', getUserById);
 router.get('/getAllUsers', getAllUsers);
 
