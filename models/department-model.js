10756224a568ecfbdefe48f0af51f52cea00196c
BrindaS11 <85933206+BrindaS11@users.noreply.github.com>
2025-03-12 19:17:49 +0530

    Update department-model.js

--- models/department-model.js
+++ models/department-model.js
@@ -2,8 +2,8 @@ module.exports = {
     tableName: "department",
     schema: {
         id: "INT AUTO_INCREMENT PRIMARY KEY",
-        dept_name: "VARCHAR(255) NOT NULL",
-        manager: "VARCHAR(255) UNIQUE NOT NULL",
+        dept_name: "VARCHAR(255)",
+        manager: "VARCHAR(255)",
         created_at: "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
     }
 };
